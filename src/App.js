1e075600031cae6d3006336ca096cd22efa8bf92
Ishakikani9117 <ishakikani999@gmail.com>
2020-05-22 16:59:17 +0530

    added files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
-
+import { Cards, Chart, CountryPicker } from './components';
+import styles from './App.module.css';
 class App extends Component{
   render(){
   return (
     <div className="App">
-    
+    <h1>App</h1>
+    <Cards />
+    <CountryPicker />
+    <Chart />
     </div>
   );
  }
